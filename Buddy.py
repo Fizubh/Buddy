e32e36f67cbd1ea5f00592782115446b9780cf25
Fizubh <>
2024-08-07 23:15:29 -0400

    Updated: Buddy.py, README.md

--- Buddy.py
+++ Buddy.py
@@ -1,15 +1,24 @@
 import os
-
+import requests
 
 from groq import Groq
 
+groq_api_key = os.environ.get("GROQ_API_KEY")
+
 
 def main():
-    groq_loader()    
+    for model in groq_models().split():
+        print(model)
+    name = input("Choose Model: ")
+    groq_loader(model_name=name)
     
 
-def groq_loader():    
-    client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+def groq_loader(model_name: str):
+    if model_name not in groq_models():
+        print(f"Cannot find model: {model_name}")
+        return
+
+    client = Groq(api_key=groq_api_key)
 
     chat_completion = client.chat.completions.create(
         messages=[
@@ -18,10 +27,26 @@ def groq_loader():
                 "content": "Explain how to write a window in Python",
             }
         ],
-        model="llama3-8b-8192",
+        model=model_name,
     )
 
-    print(chat_completion.choices[0].message.content) 
+    print(chat_completion.choices[0].message.content)
+
+
+def groq_models():
+    url = "https://api.groq.com/openai/v1/models"
+
+    headers = {
+        "Authorization": f"Bearer {groq_api_key}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(url, headers=headers).json()
+    output = ""
+    for data in response["data"]:
+        #print(data["id"])
+        output += data["id"] + " "        
+    return output        
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1 align="center">
+<p align="center">
 <pre>
    ___          __   __    
   / _ )__ _____/ /__/ /_ __
@@ -6,4 +6,26 @@
 /____/\_,_/\_,_/\_,_/\_, /
                     /___/
 </pre>
-</h1>
+</p>
+
+<h1>Setup</h1>
+
+<p>
+Create a groq account: https://groq.com/
+Create a groq api key: https://console.groq.com/keys
+
+Create environmental variable:
+
+Unix/Linux:
+In a terminal type: export GROQ_API_KEY=<your-api-key-here>
+
+Windows: (Depending on version something like)
+Control Panel > System and Security > System > Advance System Settings > Environment Variables
+Under system variables click New Type in GROQ_API_KEY for name and the key for value
+
+Install Python: https://www.python.org/downloads/
+
+In a Command-prompt/Terminal type: pip install groq
+
+Download Buddy.py and navigate to it's directory in Terminal then type: python Buddy.py
+<p>
